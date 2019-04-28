9b1fdfb2fb55566253e1e81157ddac5480eef4a4
BagasMuharom <bagashidayat45@gmail.com>
2019-04-28 18:30:37 +0700

    Update dashboard asesor

--- resources/views/dashboard/penilaian.blade.php
+++ resources/views/dashboard/penilaian.blade.php
@@ -3,7 +3,7 @@
         <div class="card-body d-flex justify-content-between align-items-center">
             <div>
                 <span class="h4 d-block font-weight-normal mb-2">{{ GlobalAuth::user()->getUjiAsAsesor()->count() }}</span>
-                <span class="font-weight-light">Pengujian yang Belum Dilakukan</span>
+                <span class="font-weight-light">Jumlah Total Pengujian</span>
             </div>
 
             <div class="h2 text-muted">
