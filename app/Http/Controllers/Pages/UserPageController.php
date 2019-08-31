0ec22aef83dabf91e75b4066142d2e5e0a2479b8
BagasMuharom <bagashidayat45@gmail.com>
2019-08-31 19:20:16 +0700

    Test detail user

--- app/Http/Controllers/Pages/UserPageController.php
+++ app/Http/Controllers/Pages/UserPageController.php
@@ -63,6 +63,8 @@ class UserPageController extends Controller
             storage_path('app/data/user/' . $user->id)
         );
 
+        return $user->id;
+
         return view('menu.user.detail', [
             'user' => $user,
             'daftarRole' => $user->getUserRole(false),
