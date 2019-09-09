6f3b18ce309a39c820789bd974ee355ab80c5a66
Rafy <rafy683@gmail.com>
2019-09-09 16:08:40 +0700

    maps dinamis

--- app/Http/Controllers/Pages/KustomisasiPageController.php
+++ app/Http/Controllers/Pages/KustomisasiPageController.php
@@ -20,18 +20,20 @@ class KustomisasiPageController extends Controller
     public function index()
     {
         return view('menu.kustomisasi.index', [
-            'menu' => Menu::findByRoute(Menu::SKEMA), //
-            'nama' => Kustomisasi::find(Kustomisasi::NAMA), //
-            'logo' => Kustomisasi::find(Kustomisasi::LOGO), //
+            'menu' => Menu::findByRoute(Menu::SKEMA),
+            'nama' => Kustomisasi::find(Kustomisasi::NAMA),
+            'logo' => Kustomisasi::find(Kustomisasi::LOGO),
             'profil' => Kustomisasi::find(Kustomisasi::PROFIL),
-            'visi' => Kustomisasi::find(Kustomisasi::VISI), //
-            'misi' => Kustomisasi::find(Kustomisasi::MISI), //
-            'sasaranMutu' => Kustomisasi::find(Kustomisasi::SASARAN_MUTU), //
-            'susunanOrganisasi' => Kustomisasi::find(Kustomisasi::SUSUNAN_ORGANISASI), //
-            'email' => Kustomisasi::find(Kustomisasi::EMAIL), //
-            'noTelp' => Kustomisasi::find(Kustomisasi::NO_TELP), //
-            'alamat' => Kustomisasi::find(Kustomisasi::ALAMAT), //
-            'pengumuman' => Kustomisasi::find(Kustomisasi::PENGUMUMAN) //
+            'visi' => Kustomisasi::find(Kustomisasi::VISI),
+            'misi' => Kustomisasi::find(Kustomisasi::MISI),
+            'sasaranMutu' => Kustomisasi::find(Kustomisasi::SASARAN_MUTU),
+            'susunanOrganisasi' => Kustomisasi::find(Kustomisasi::SUSUNAN_ORGANISASI),
+            'email' => Kustomisasi::find(Kustomisasi::EMAIL),
+            'noTelp' => Kustomisasi::find(Kustomisasi::NO_TELP),
+            'alamat' => Kustomisasi::find(Kustomisasi::ALAMAT),
+            'pengumuman' => Kustomisasi::find(Kustomisasi::PENGUMUMAN),
+            'maps' => Kustomisasi::find(Kustomisasi::MAPS),
+            'mapsUrl' => Kustomisasi::find(Kustomisasi::MAPS_URL)
         ]);
     }
 }
