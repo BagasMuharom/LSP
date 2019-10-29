d3950adac2c9507c564f7e903f892bb6c6a7e8ff
BagasMuharom <bagashidayat45@gmail.com>
2019-10-30 06:58:55 +0700

    Mahasiswa bisa melihat sertifikat

--- app/Http/Controllers/SertifikatController.php
+++ app/Http/Controllers/SertifikatController.php
@@ -17,10 +17,10 @@ class SertifikatController extends Controller
     public function __construct()
     {
         $this->middleware(['auth:user', 'menu:sertifikat'])->except([
-            'isiKuesioner'
+            'isiKuesioner', 'lihatBerkasSertifikat'
         ]);
 
-        $this->middleware(['auth:mhs'])->only(['isiKuesioner']);
+        $this->middleware(['auth:mhs'])->only(['isiKuesioner', 'lihatBerkasSertifikat']);
     }
 
     /**
